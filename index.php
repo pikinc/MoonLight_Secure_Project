53d46b5b415f8300514be22c566b6538a52ae482
PIK Inc. Yoshihiro Murai <33089109+pikinc@users.noreply.github.com>
2024-09-15 19:49:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="./common/css/style.css?t=<?=time()?>">
 <title></title>
 </head>
 <body>
